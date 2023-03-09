1fe5115f92e2aeb5e2350593195ffda40fc041ae
Gracia <dgracia@teksystems.com>
2023-03-09 12:48:44 -0600

    added springboot to project.

---
+++ service/uberEatsClone/src/main/java/com/example/uberEatsClone/UberEatsCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberEatsClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberEatsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberEatsCloneApplication.class, args);
+	}
+
+}
---
+++ service/uberEatsClone/src/test/java/com/example/uberEatsClone/UberEatsCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uberEatsClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberEatsCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
